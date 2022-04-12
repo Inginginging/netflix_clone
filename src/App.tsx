82038eec5d925d42860ba458d43ec18908ccc5d2
ing928 <kik928@nate.com>
2022-04-12 16:07:01 +0900

    slider (AnimatePresence, custom)

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
   background: linear-gradient(135deg, #e09, #d0e);
+  flex-direction: column;
 `;
 const Box = styled(motion.div)`
   width: 400px;
@@ -18,31 +19,50 @@ const Box = styled(motion.div)`
   position: absolute;
   top: 100px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
 `;
 
 const boxVariants = {
-  initial: { opacity: 0, scale: 0, y: 50 },
-  visible: { opacity: 1, scale: 1, y: 0 },
-  exit: { opacity: 0, scale: 0, y: 50 },
+  initial: (back: boolean) => ({ x: back ? -500 : 500, opacity: 0, scale: 0 }),
+  visible: { opacity: 1, scale: 1, x: 0, transition: { duration: 1 } },
+  exit: (back: boolean) => ({
+    x: back ? 500 : -500,
+    opacity: 0,
+    scale: 0,
+    transition: { duration: 1 },
+  }),
 };
 function App() {
-  const [showing, setShowing] = useState(false);
-  const handleShowing = () => {
-    setShowing((prev) => !prev);
+  const [visible, setVisible] = useState(1);
+  const [back, setBack] = useState(false);
+  const handleNext = () => {
+    setBack(false);
+    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+  };
+  const handlePrev = () => {
+    setBack(true);
+    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
   };
   return (
     <Wrapper>
-      <AnimatePresence>
-        {showing ? (
-          <Box
-            variants={boxVariants}
-            initial="initial"
-            animate="visible"
-            exit="exit"
-          />
-        ) : null}
+      <AnimatePresence exitBeforeEnter custom={back}>
+        <Box
+          /*custom: 각 애니메이션 component에 대해 variants를 다르게 적용할때 사용. => variants를 함수로 return */
+          custom={back}
+          key={visible}
+          variants={boxVariants}
+          initial="initial"
+          animate="visible"
+          exit="exit"
+        >
+          {visible}
+        </Box>
       </AnimatePresence>
-      <button onClick={handleShowing}>click</button>
+      <button onClick={handleNext}>next</button>
+      <button onClick={handlePrev}>prev</button>
     </Wrapper>
   );
 }
