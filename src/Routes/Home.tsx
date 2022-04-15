a11d027f68cc0f0fef91415f45c15a4f60f8c053
ing928 <kik928@nate.com>
2022-04-15 18:08:12 +0900

    Slider (pagination, leaving state)

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,3 +1,5 @@
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
 import { useQuery } from "react-query";
 import styled from "styled-components";
 import { getMovies, IGetMovies } from "../api";
@@ -5,6 +7,7 @@ import { makeImgPath } from "../utils";
 
 const Wrapper = styled.div`
   background: black;
+  padding-bottom: 200px;
 `;
 const Loader = styled.div`
   height: 20vh;
@@ -23,7 +26,7 @@ const Banner = styled.div<{ bgPhoto: string }>`
   background-size: cover;
 `;
 const Title = styled.h2`
-  font-size: 68px;
+  font-size: 78px;
   font-weight: 500;
   margin-bottom: 20px;
 `;
@@ -31,23 +34,96 @@ const Overview = styled.p`
   font-size: 30px;
   width: 50%;
 `;
+const Slider = styled.div`
+  position: relative;
+  top: -100px;
+`;
+const Row = styled(motion.div)`
+  position: absolute;
+  display: grid;
+  grid-template-columns: repeat(9, 1fr);
+  gap: 5px;
+  width: 100%;
+`;
+const Box = styled(motion.div)<{ bgPhoto: string }>`
+  background-color: white;
+  background-image: url(${(props) => props.bgPhoto});
+  background-size: cover;
+  background-position: center center;
+  height: 250px;
+  font-size: 40px;
+`;
+
+const rowVariants = {
+  initial: {
+    x: window.innerWidth,
+  },
+  animate: { x: 0 },
+  exit: {
+    x: -window.innerWidth,
+  },
+};
+
+const offset = 9; //slider page의 movie갯수
 
 function Home() {
   const { data, isLoading } = useQuery<IGetMovies>(
     ["movies", "nowPlaying"],
     getMovies
   );
-
+  //row의 index를 관찰하는 state
+  const [index, setIndex] = useState(0);
+  const [leaving, setLeaving] = useState(false); //slider가 움직이는 상태인지 확인하는 state
+  const toggleLeaving = () => setLeaving((prev) => !prev);
+  const increaseIndex = () => {
+    if (data) {
+      if (leaving) return; //slider가 움직이고 있으면 index를 increase하지 않음
+      toggleLeaving(); //slider가 움직이지 않은 상태면 leaving state를 true로 toggle
+      const totalMovies = data.results.length - 1; //첫번째 movie는 대문 poster로 사용
+      const maxPage = Math.floor(totalMovies / offset) - 1;
+      setIndex((prev) => (prev === maxPage ? 0 : prev + 1)); //maxpage이면 다시 처음으로 회귀
+    }
+  };
   return (
     <Wrapper>
       {isLoading ? (
         <Loader>Loading...</Loader>
       ) : (
         <>
-          <Banner bgPhoto={makeImgPath(data?.results[0].backdrop_path || "")}>
+          <Banner
+            onClick={increaseIndex}
+            bgPhoto={makeImgPath(data?.results[0].backdrop_path || "")}
+          >
             <Title>{data?.results[0].title}</Title>
             <Overview>{data?.results[0].overview}</Overview>
           </Banner>
+          <Slider>
+            <AnimatePresence
+              initial={false}
+              /*처음 animate를 무시하는 prop*/ onExitComplete={
+                toggleLeaving
+              } /*slider의 animation이 멈추는것을 막아줌.*/
+            >
+              <Row
+                variants={rowVariants}
+                initial="initial"
+                animate="animate"
+                exit="exit"
+                transition={{ type: "tween", duration: 1 }}
+                key={index}
+              >
+                {data?.results
+                  .slice(1)
+                  .slice(offset * index, offset * index + offset)
+                  .map((movie) => (
+                    <Box
+                      key={movie.id}
+                      bgPhoto={makeImgPath(movie.poster_path)}
+                    />
+                  ))}
+              </Row>
+            </AnimatePresence>
+          </Slider>
         </>
       )}
     </Wrapper>
